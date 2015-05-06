f85724a7a63877ea2959dc78ceb79bb8ad66cb00
Peter Berezny <spontain@gmail.com>
2015-05-06 11:50:47 +0200

    Update config template with URL

--- conf/config_template.js
+++ conf/config_template.js
@@ -3,12 +3,19 @@
 module.exports = {
   // Environment specific
   production: {
-    port: 3000,
-    storage_dir: "/tmp"
+    port: 3337,
+    storage_dir: "/tmp",
+    url: "https://www.example.com/"
   },
+  test: {
+    port: 3331,
+    storage_dir: "/tmp",
+    url: "https://www.example-test.com/"
+  },  
   development: {
     port: 3000,
-    storage_dir: "/tmp"
+    storage_dir: "/tmp",
+    url: "https://localhost/"
   },
   // Non-env specific
   secret: 'XXXXX',
