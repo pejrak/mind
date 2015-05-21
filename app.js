750644149e9823fde8df29492dc221ccee2d6121
Peter Berezny <spontain@gmail.com>
2015-05-21 09:52:00 +0200

    WIP

--- app.js
+++ app.js
@@ -7,18 +7,13 @@ var express = require('express')
 var app = express()
 var common = require('./lib/common')
 var LOG = common.LOG('app')
-var environment = (process.env.NODE_ENV || 'development')
 var port = (process.env.PORT || 3000)
 
 // Create MIND object to pass around and assign components to it
 var MIND = {
       name: 'Mind',
-      env: environment,
-      environment: environment,
-      config: require('./conf/config'),
       app: app,
       express: express,
-      LOG: common.LOG,
       common: common,
       dir: __dirname
     }
@@ -26,9 +21,10 @@ var MIND = {
 // Load core for initialization
 MIND.core = require('./lib/core')(MIND)
 MIND.core.init()
-// Start server listening
+
+// Start http server listening
 app.listen(port, function() {
-  LOG('App on port:', port)
+  LOG('(' + MIND.name + ') application listening on port:', port)
 })
 
 
