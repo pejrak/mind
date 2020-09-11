461cb85f75c045cf813c9cd65d2ad12d3257092a
Peter Berezny <spontain@gmail.com>
2020-09-11 09:43:04 +0200

    wip

--- lib/ui/main/Main.vue
+++ lib/ui/main/Main.vue
@@ -1,4 +1,22 @@
 <template lang="pug">
-div
-  h1 Hello
+b-navbar(
+  href="#"
+  variant="primary"
+  type="dark"
+)
+  b-navbar-brand Mind
+  b-navbar-toggle(target="nav-collapse")
+  b-collapse(id="nav-collapse" is-nav)
+    main-navigation
 </template>
+
+<script>
+
+import MainNavigation from '../navigation/MainNavigation.vue'
+
+export default {
+  components: {
+    MainNavigation,
+  }
+}
+</script>
---
+++ lib/ui/navigation/MainNavigation.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+div
+  b-navbar-nav
+    b-nav-item(href="/login") Login
+    b-nav-item(href="#") Load
+    b-nav-item(href="#") Extract
+</template>
+<script>
+export default {
+
+}
+</script>
